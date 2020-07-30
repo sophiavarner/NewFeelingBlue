fd0398f5aad18aa594abb1af8658e52126039975
sophiavarner <53187303+sophiavarner@users.noreply.github.com>
2020-07-30 08:50:01 -0700

    Add files via upload

---
+++ Feeling Blue/Feeling Blue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Feeling Blue
+//
+//  Created by Maya stein on 7/28/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Feeling Blue/Feeling BlueTests/Feeling_BlueTests.swift
@@ -0,0 +1,34 @@
+//
+//  Feeling_BlueTests.swift
+//  Feeling BlueTests
+//
+//  Created by Maya stein on 7/28/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import XCTest
+@testable import Feeling_Blue
+
+class Feeling_BlueTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
